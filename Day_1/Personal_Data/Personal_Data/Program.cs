e3c99259649b0f5523e23274ac8ab5959cc2a8cb
amratef2005 <atef45692@gmail.com>
2026-02-01 22:50:17 +0200

    Personal_Data

---
+++ Day_1/Personal_Data/Personal_Data/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Enter Your Age: ");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter your City: ");
+string city = Console.ReadLine();
+Console.WriteLine($"Name:'{name}'\n");
+Console.WriteLine($"Age:'{age}'\n");
+Console.WriteLine($"City:'{city}'\n");
