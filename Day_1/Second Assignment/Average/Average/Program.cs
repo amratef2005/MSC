0f75b9e5307b503c8f6d76d0990ca5f69b1adbd8
amratef2005 <atef45692@gmail.com>
2026-02-02 16:32:41 +0200

    End of task 1

---
+++ Day_1/Second Assignment/Average/Average/Program.cs
@@ -0,0 +1,10 @@
+﻿decimal num1,num2,num3,average; 
+Console.Write("Enter first number: ");
+num1 = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Enter second number: ");
+num2 = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Enter third number: ");
+num3 = Convert.ToDecimal(Console.ReadLine());
+average = (num1 + num2 + num3) / 3;   
+Console.WriteLine($"The average =  {average}\n");
+
---
+++ Day_2/Fictional Character/Fictional Character/Fictional Character/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string Name, favorite_hobby;
+        int age;
+        bool is_student=true ;
+        Console.WriteLine(@"Enter Your Name :");
+        Name=Console.ReadLine();
+        Console.WriteLine(@"Enter Your Age :");
+        age=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(@"Enter Your Favorite Hobby :");
+        favorite_hobby=Console.ReadLine();
+        Console.WriteLine("Are you a student?(True / False)");
+        is_student=Convert.ToBoolean(Console.ReadLine());
+
+        Console.WriteLine($"\"Hi, my name is {Name}. I am {age} years old and it is {is_student} that I am a student. My favorite hobby is {favorite_hobby}!\"");
+
+       
+
+    }
+}
