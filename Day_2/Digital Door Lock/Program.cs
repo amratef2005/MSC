1d871759d3dd7430bc57fc05e20da9a7ed84ebe8
amratef2005 <atef45692@gmail.com>
2026-02-05 23:33:05 +0200

    End of assignment

---
+++ Day_2/Digital Door Lock/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int user_age, required_age=18;
+
+        Console.Write("Enter your age: ");
+        user_age = Convert.ToInt32(Console.ReadLine());
+
+        if (user_age >= required_age)
+        {
+            Console.WriteLine("You Are Eligible To Vote.");
+        }
+        else
+        {
+            Console.WriteLine("You Are Not Eligible To Vote.");
+        }
+
+    }
+
+}
