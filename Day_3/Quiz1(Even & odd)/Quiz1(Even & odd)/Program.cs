1067587186e107b7b24632fd3a93c2d98901e069
amratef2005 <atef45692@gmail.com>
2026-02-08 23:13:12 +0200

    Day_3

---
+++ Day_3/Quiz1(Even & odd)/Quiz1(Even & odd)/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter Number : ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine($"{num} is Even Number");
+}
+else
+{
+    Console.WriteLine($"{num} is Odd Number");
+}
