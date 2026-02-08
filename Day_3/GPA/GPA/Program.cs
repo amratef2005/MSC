37137bed446033db2333aca9cd17a870456b9362
amratef2005 <atef45692@gmail.com>
2026-02-08 22:41:40 +0200

    Day_3

---
+++ Day_3/GPA/GPA/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("-> Highest value 4 <-");
+
+Console.Write(("Enter Your GPA :"));
+double GPA = double.Parse( Console.ReadLine());
+
+if (GPA <= 4 && GPA >= 3)
+{
+    Console.WriteLine("The Estimate Of GPA is = [A+]");
+}
+else if (GPA < 3 && GPA >= 2)
+{
+    Console.WriteLine("The Estimate Of GPA is = [A]");
+}
+else if (GPA < 2 && GPA >= 1.5)
+{
+    Console.WriteLine("The Estimate Of GPA is = [B]");
+}
+else if (GPA < 1.5 && GPA > 0)
+{
+    Console.WriteLine("The Estimate Of GPA is = [c]");
+}
+else Console.WriteLine("Please enter the correct value\n");
